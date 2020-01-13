6746aebdc89a9ccbf562aa39b40013cc93e87216
CyberCat3 <andreas17349@gmail.com>
2020-01-13 20:52:36 +0100

    Removed typescript test.

--- scripts/test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var hej = 5;
-var dav = "21";
-console.log(hej + "" + dav);
--- scripts/test.ts
+++
@@ -1,4 +0,0 @@
-var hej: number = 5;
-var dav: String = "21";
-var goddag: String = "hsahahah";
-console.log(hej + "" + dav + goddag);
