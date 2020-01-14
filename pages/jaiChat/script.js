436d52c49d6339451130284bdff49b9228be0209
CyberCat3 <andreas17349@gmail.com>
2020-01-15 00:00:00 +0100

    Made the sending part and added more animations.

--- pages/jaiChat/script.js
+++ pages/jaiChat/script.js
@@ -1,5 +1,13 @@
 const textArea = document.querySelector(".chat-app.sender textarea");
 const chatAppContainer = document.querySelector(".chat-app.container");
+const sendButton = document.querySelector(".chat-app .sender .send-button");
+const sendButtonPlane = document.querySelector(".chat-app .sender .send-button img");
+
+let websocket = new WebSocket("wss://jaibel.ddns.net/ws/chat");
+
+websocket.onmessage = message => {
+    console.log(message.data);
+};
 
 textArea.addEventListener("wheel", event => {
     textArea.scrollBy(0, event.deltaY / 8);
@@ -15,9 +23,13 @@ setTimeout(() => {
     chatAppContainer.style.opacity = "100%";
 }, 10);
 
-
-chatAppContainer.addEventListener("click", event => {
-    chatAppContainer.style.width = "90vw";
-    chatAppContainer.style.height = "90vh";
+sendButtonPlane.addEventListener("animationend", () => {
+    sendButtonPlane.classList.remove("send-animation");
 });
 
+function send() {
+    sendButtonPlane.classList.add("send-animation");
+    console.log(textArea.value);
+    websocket.send(textArea.value);
+    textArea.value = "";
+}
