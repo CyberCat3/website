6b905a23b00ce4589a3cfde15da30ce50dea54e8
CyberCat3 <andreas17349@gmail.com>
2020-01-25 00:40:10 +0100

    Remember-me and JSON-parsing implemeted.

--- pages/jaiChat/js/functions.js
+++ pages/jaiChat/js/functions.js
@@ -1,6 +1,20 @@
+let loginRealForm, loginForm, loginContent;
+let signInHeader, signInContent, signInButton, signInUsername, signInPassword;
+let signUpHeader, signUpContent, signUpButton, signUpUsername, signUpPassword, signUpConfirmPassword;
+
+function hideLogin() {
+    setTimeout(() => {
+        loginForm.style.opacity = "0%";
+        loginForm.style.pointerEvents = "none";
+        setTimeout(() => {
+            loginForm.style.maxHeight = "0px"
+            loginForm.style.marginBottom = "-30px";
+        }, 400);
+    }, 100);
+}
+
 function showLogin() {
     messages.appendChild(loginTemplate.content);
-    setTimeout(() => loginForm.style.opacity = "100%", 100);
     //-------------GETS THE HTML ELEMENTS-------------//
     loginRealForm           =   docQuery(".login-real-form");
     loginForm               =   docQuery(".login-form");
@@ -11,6 +25,7 @@ function showLogin() {
     signInButton            =   docQuery(".login-form .sign-in-content .login-button");
     signInUsername          =   docQuery("#login-sign-in-username-input");
     signInPassword          =   docQuery("#login-sign-in-password-input");
+    signInRememberMe        =   docQuery("#remember-me-sign-in");
 
     signUpHeader            =   docQuery(".login-form .sign-up-header");
     signUpContent           =   docQuery(".login-form .sign-up-content");
@@ -18,6 +33,12 @@ function showLogin() {
     signUpUsername          =   docQuery("#login-sign-up-username-input");
     signUpPassword          =   docQuery("#login-sign-up-password-input");
     signUpConfirmPassword   =   docQuery("#login-sign-up-confirm-password-input");
+    signUpRememberMe        =   docQuery("#remember-me-sign-up");
+
+    //-------------SHOWS LOGIN-------------//
+    loginForm.style.removeProperty("pointer-events");
+    loginForm.style.marginBottom = "20px";
+    setTimeout(() => loginForm.style.opacity = "100%", 100);
 
     //-------------DECLARES FUNCTIONS-------------//
     /* This function checks if the form contains a valid 
@@ -62,7 +83,7 @@ function showLogin() {
     signInHeader.onclick = () => {
         if (mode === signInHeader) { return; }
         mode = signInHeader;
-        loginForm.style.maxHeight = "170px"
+        loginForm.style.maxHeight = "190px"
         setTimeout(() => {
             signUpHeader.classList.remove("login-header-activated");
             signUpContent.style.display = "none";
@@ -72,14 +93,14 @@ function showLogin() {
             signInContent.style.display = "block";
             signInUsername.required = signInPassword.required = true;
 
-            loginForm.style.maxHeight = "276px";
+            loginForm.style.maxHeight = "305px";
         }, 200);
     };
 
     signUpHeader.onclick = () => {
         if (mode === signUpHeader) { return; }
         mode = signUpHeader;
-        loginForm.style.maxHeight = "170px"
+        loginForm.style.maxHeight = "190px"
         setTimeout(() => {
             signInHeader.classList.remove("login-header-activated");
             signInContent.style.display = "none";
@@ -89,22 +110,29 @@ function showLogin() {
             signUpContent.style.display = "block";
             signUpUsername.required = signUpPassword.required = signUpConfirmPassword.required = true;
 
-            loginForm.style.maxHeight = "347px";
+            loginForm.style.maxHeight = "377px";
         }, 200);
     };
     
     signInHeader.onclick();
+    
     //-------------SETS UP FORM SUBMIT-------------//
     loginRealForm.onsubmit = () => {
         const request = {};
+        let rememberMe;
         if (mode === signInHeader) {
             request.type = "sign-in";
             request.username = signInUsername.value;
             request.password = signInPassword.value;
+            rememberMe = signInRememberMe.checked;
         } else if (mode === signUpHeader) {
             request.type = "sign-up";
             request.username = signUpUsername.value;
             request.password = signUpPassword.value;
+            rememberMe = signInRememberMe.checked;
+        }
+        if (rememberMe) {
+            localStorage.setItem("jaiChat-loginInfo", JSON.stringify({username: request.username, password: request.password}));
         }
         console.log("request: ",request);
         websocket.send(JSON.stringify(request));
@@ -121,20 +149,48 @@ function setupWebsocket() {
         websocket = new WebSocket("wss://jaibel.ddns.net/ws/chat");
         console.log("connecting to jaibel servers");
     }
-    console.log(`Websocket ready state: ${wsReadyStateString(websocket.readyState)}`);
 
     websocket.onopen = () => {
-        console.log(`Websocket ready state: ${wsReadyStateString(websocket.readyState)}`);
-        online();
+        try { 
+            const localLogin = JSON.parse(localLoginRawString);
+            if (localLogin && localLogin.username && localLogin.password) {
+                websocket.send(JSON.stringify({type: "sign-in", username: localLogin.username, password: localLogin.password})); 
+            }
+        } catch {}
         showLogin();
+        online();
     }
     websocket.onclose = () => {
-        console.log(`Websocket ready state: ${wsReadyStateString(websocket.readyState)}`);
         offline();
     }
 
-    websocket.onmessage = message => {
-        receiveMessage("Some Human", message.data, new Date().getTime(), Math.random() > 0.5);
+    let myName;
+
+    websocket.onmessage = data => {
+        const response = JSON.parse(data.data);
+        console.log(`Received message: ${data.data}`);
+        switch (response.type) {
+            case "message": {
+                receiveMessage(response.sender, response.content, response.time, myName === response.sender);
+                break;
+            }
+            case "sign-in-confirmed": {
+                myName = response.username;
+                hideLogin();
+                break;
+            }
+            case "sign-up-confirmed": {
+                myName = response.username;
+                hideLogin();
+                break;
+            }
+            case "sign-in-denied": {
+                break;
+            }
+            case "sign-up-denied": {
+                break;
+            }
+        }
     };
 }
 
@@ -143,16 +199,18 @@ function receiveMessage(person, messageContent, unixEpochTimeMillis, isRight) {
     messages.appendChild(messageElement);
     // The animation doesn't work, if this timeout isn't here.
     setTimeout(() => messages.lastElementChild.style.opacity = "100%", 0);
+    
     const distFromBottom = messages.scrollHeight - messages.clientHeight - messages.scrollTop;
-    if (distFromBottom < 200) {
+    if (distFromBottom < 400) {
         messages.scrollBy(0, distFromBottom - 20);
     }
 }
 
 function send() {
-    if (isOnline && textArea.value.trim().length) { // If length is zero, it's not "truthy"
+    if (isOnline && isLoggedIn && textArea.value.trim().length) { // If length is zero, it's not "truthy"
         sendButtonPlane.classList.add("send-animation");
-        websocket.send(JSON.stringify({type: "message", message: textArea.value}));
+        websocket.send(JSON.stringify({type: "message", content: textArea.value}));
+        console.log(`sending: ${JSON.stringify({type: "message", content: textArea.value})}`);
         textArea.value = "";
     }
 }
@@ -193,13 +251,4 @@ function createMessageElement(person, messageContent, date, isRight, isTheMessag
     const template = document.createElement("template");
     template.innerHTML = html;
     return template.content;
-}
-
-function wsReadyStateString(readyState) {
-    switch (readyState) {
-        case   WebSocket.CONNECTING:   return   "CONNECTING";
-        case   WebSocket.OPEN:         return   "OPEN";
-        case   WebSocket.CLOSING:      return   "CLOSING";
-        case   WebSocket.CLOSED:       return   "CLOSED";
-    }
 }
